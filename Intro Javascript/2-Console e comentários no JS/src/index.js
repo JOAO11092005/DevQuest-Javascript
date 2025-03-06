b53e04bbded33a0862a8a56f80ffc2355ebc84a7
João Paulo Neves Batista <joaopaulonevesbatista@gmail.com>
2025-03-06 18:43:27 -0300

    Atualizar Curso Upload

---
+++ Intro Javascript/2-Console e comentários no JS/src/index.js
@@ -0,0 +1,7 @@
+console.log('Olá Mundo');
+let a = window.prompt("Digite o seu nome:");
+
+let b = document.querySelector('p');
+console.log(b);
+let nome = a;
+b.innerText = `Olá seja bem vindo ${nome}`;

d2a75b0b70e7da9232ab16874d8872671de9d990
João Paulo Neves Batista <joaopaulonevesbatista@gmail.com>
2025-03-07 15:28:27 -0300

    Atualizar Curso Upload

---
+++ Javascript Básico/3- Variaveis/index.js
@@ -0,0 +1,9 @@
+var pessoa =  "Joao Paulo";
+const  emailPessoa = "joao@gmail.com";
+//emailPessoa = "Joaopaulo@gmail.com"; o valor não altera.
+
+let telefonePessoa = "62992576354";
+
+telefonePessoa = "Sem Numero";
+
+console.log(telefonePessoa);
---
+++ Javascript Básico/4-tipos-primitivos/index.js
@@ -0,0 +1,7 @@
+let nome = 'Roberto'; //tipo string
+let numero = 123; //tipo number
+let vOuF = true; //tipo Boleano verdadeiro ou falso
+let pessoa = null; 
+let telefone = undefined; //tipo indefinido
+
+console.log(telefone);
